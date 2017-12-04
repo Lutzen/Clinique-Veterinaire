6bb4a6ebeaa49d865f3a25d5cdae8a53ef48b49a
aphommaline2017 <aphommaline2017@44-6.ad.campus-eni.fr>
2017-12-04 14:59:57 +0100

    jdbc tools

--- src/fr/eni/cliniqueVeterinaire/dal/JdbcTools.java
+++ src/fr/eni/cliniqueVeterinaire/dal/JdbcTools.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import cliniqueVeterinaire.dal.Settings;
+import fr.eni.cliniqueVeterinaire.dal.Settings;
 
 
 
