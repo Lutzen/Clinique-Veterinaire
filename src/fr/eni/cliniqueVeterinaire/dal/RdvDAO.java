2cc6fc33de6e3027cebd35ecb02090f2ec26acaf
aphommaline2017 <aphommaline2017@44-6.ad.campus-eni.fr>
2017-12-04 16:27:42 +0100

    RdvDAO

--- src/fr/eni/cliniqueVeterinaire/dal/RdvDAO.java
+++ src/fr/eni/cliniqueVeterinaire/dal/RdvDAO.java
@@ -1,10 +1,9 @@
 package fr.eni.cliniqueVeterinaire.dal;
 
-import java.sql.Date;
+
 import java.util.List;
 
-import fr.eni.cliniqueVeterinaire.bo.Animal;
-import fr.eni.cliniqueVeterinaire.bo.Client;
+
 import fr.eni.cliniqueVeterinaire.bo.Rdv;
 
 //import cliniqueVeterinaire.bo.Article;
@@ -14,11 +13,11 @@ public interface RdvDAO {
 
 	List<Rdv> selectById(int codeVeto) throws DALException;
 
-	void insert(Client client,Animal animal) throws DALException;
+	void insert(Rdv rdv) throws DALException;
 
-	void update(int codeVeto,Rdv dateRdv,Animal animal,Date newDate) throws DALException;
+	void update(Rdv rdv) throws DALException;
 
-	void delete(int codeVeto,Date dateRdv,Animal animal) throws DALException;
+	void delete(Rdv rdv) throws DALException;
 
 	
 	
